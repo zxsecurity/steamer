de2954b720fca7e7cbf63a0b72a9f8f3d57ccca0
Nathan Cobbald <nathan.cobbald@zxsecurity.co.nz>
2023-04-04 13:52:14 +1200

    Fixing importer

--- importers/lastfm.go
+++ importers/lastfm.go
@@ -10,14 +10,11 @@ type TemplateLineParser struct{}
 
 func (t TemplateLineParser) ParseLine(line string) ([]interface{}, error) {
 	entries := make([]interface{}, 0)
-	// ...change this to split a line in the text file
-	data := util.SplitString(line, ':', true, true)
-	// ...change this to check data here
+	data := util.SplitString(line, ',', true, true)
 	if len(data) < 4 {
 		entries = append(entries, interface{}(nil))
 		return entries, nil
 	}
-	// ...change this to extract the relevant data fields to form an entry
 	entry := util.GenericData{
 		Email:        data[1],
 		Liame:        util.Reverse(data[1]),
@@ -31,13 +28,11 @@ func (t TemplateLineParser) ParseLine(line string) ([]interface{}, error) {
 
 // EstimateCount estimates how many entries are in a line (for the progress bar)
 func (t TemplateLineParser) EstimateCount(line string) (int, error) {
-	// We have one cred per line by default
 	return 1, nil
 }
 
 func main() {
 	parser := TemplateLineParser{}
-	// ...change filename to the default location of the dump file
 	importer, err := util.MakeImporter("/dumps/lastfm.txt", parser, 15)
 	if err != nil {
 		fmt.Println(err)
