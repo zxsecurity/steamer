cc2421fa95a19cc881eda4d83788f9e8392b4b72
Nathan Cobbald <77047843+Real4XoR@users.noreply.github.com>
2023-05-02 13:22:51 +1200

    Create lifeboat.go

---
+++ importers/lifeboat.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+        "fmt"
+
+        "github.com/zxsecurity/steamer/importers/util"
+)
+
+type TemplateLineParser struct{}
+
+func (t TemplateLineParser) ParseLine(line string) ([]interface{}, error) {
+        entries := make([]interface{}, 0)
+        data := util.SplitString(line, '\t', true, true)
+        if len(data) < 4 {
+                entries = append(entries, interface{}(nil))
+                return entries, nil
+        }
+        entry := util.GenericData{
+                Email:        data[1],
+                Liame:        util.Reverse(data[1]),
+                PasswordHash: data[2],
+                Username:     data[0],
+                Breach:       "LifeBoat2016",
+        }
+        fmt.Println(data)
+        entries = append(entries, entry)
+        return entries, nil
+}
+
+// EstimateCount estimates how many entries are in a line (for the progress bar)
+func (t TemplateLineParser) EstimateCount(line string) (int, error) {
+        return 1, nil
+}
+
+func main() {
+        parser := TemplateLineParser{}
+        importer, err := util.MakeImporter("/dumps/lifeboat.txt", parser, 15)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
+        importer.Run()
+        importer.Finish()
+}
